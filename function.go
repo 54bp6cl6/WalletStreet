2536c181b14b7fead0496b8ddf7f147a02e3fe88
Max Zheng <54bp6cl6@gmail.com>
2021-09-28 20:11:47 +0800

    Revert "整理程式碼以增加可讀性"
    This reverts commit c0c1eb2dbf32078d6179ab4f553c0f8e7bcf6b34.

--- function.go
+++ function.go
@@ -1,6 +1,10 @@
 package function
 
 import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 	"regexp"
 	"strings"
 
@@ -9,6 +13,68 @@ import (
 	"github.com/54bp6cl6/WalletStreet/ui"
 	"github.com/line/line-bot-sdk-go/linebot"
 )
+
+var (
+	bot *linebot.Client
+)
+
+func init() {
+	var err error
+	bot, err = linebot.New(os.Getenv("CHANNEL_SECRET"), os.Getenv("CHANNEL_TOKEN"))
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+}
+
+// Cloud Function 進入點
+func Webhook(w http.ResponseWriter, r *http.Request) {
+	db.Connect()
+	defer db.Close()
+	events, err := bot.ParseRequest(r)
+	if err != nil {
+		if err == linebot.ErrInvalidSignature {
+			w.WriteHeader(400)
+		} else {
+			w.WriteHeader(500)
+		}
+		fmt.Fprint(w, err)
+		return
+	}
+
+	for _, event := range events {
+		err = HandleEvent(event)
+		if err != nil {
+			w.WriteHeader(400)
+			fmt.Fprint(w, err)
+			return
+		}
+	}
+
+	fmt.Fprint(w, "OK")
+}
+
+// Bot 邏輯的起始點
+func HandleEvent(event *linebot.Event) (err error) {
+	err = HandleFollowEvent(event)
+	return
+}
+
+// 處理加入好友與封鎖事件的中間層
+func HandleFollowEvent(event *linebot.Event) (err error) {
+	switch event.Type {
+	case linebot.EventTypeFollow:
+		_, err = bot.ReplyMessage(event.ReplyToken, ui.FollowMessage()).Do()
+		return
+	case linebot.EventTypeUnfollow:
+		// TODO: 刪除所有使用者資料
+		return
+	}
+
+	// 進入下一層
+	err = HandleJoinGameEvent(event)
+	return
+}
 
 // 處理創建與加入遊戲的中間層
 func HandleJoinGameEvent(event *linebot.Event) (err error) {
@@ -37,6 +103,7 @@ func HandleJoinGameEvent(event *linebot.Event) (err error) {
 
 		// 創建新遊戲
 		if data[postback.Action] == postback.CreateGame {
+			// 生成遊戲編號
 			var gameId string
 			if gameId, err = db.CreateGame(event.Source.UserID); err != nil {
 				_, err = bot.ReplyMessage(event.ReplyToken, ui.ErrorMessage(err)).Do()
